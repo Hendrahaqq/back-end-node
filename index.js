df63f204c54d48c9958844160fa7a106317c3c65
Muhammad Hendra Nailul Haqq <hendrahaqq@gmail.com>
2021-07-06 22:07:02 +0700

    create get endpoint from flickr

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const fetch = require('node-fetch')
+const app = express()
+
+app.get('/get', async (req, res) => {
+  try {
+
+    const url = 'https://api.flickr.com/services/feeds/photos_public.gne?tagmode=any&format=json&nojsoncallback=callback'
+    const tagQuery = req.query.tag
+    const apiResponse = await fetch(
+      tagQuery != '' ? url + '&tags=' + tagQuery : url
+    )
+    const apiResponseJson = await apiResponse.json()
+    
+    res.send(apiResponseJson.items)
+  } catch (err) {
+    console.log(err)
+    res.status(500).send('Something went wrong')
+  }
+})
+
+app.listen(3000, () => console.log(`Example app listening on port 3000!`))
